aab42b12468dcc73f3ee3e196c7093a38a0ad5ec
JCorley44 <jcorley44@gmail.com>
2021-12-15 08:53:56 -0600

    Add files via upload

---
+++ src/components/BlogData.js
@@ -0,0 +1,29 @@
+export const Blogs = [
+  {
+    id: 1,
+    name: `What is "this"?`,
+    tags: ["javascript", "this"],
+    date: "28 Jun 2020",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--9yrB8K1n--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/3rwl84ha2k4va38fr87q.jpg",
+    link: "https://dev.to/jcorley44/this-or-that-object-understanding-this-1j0l",
+  },
+  {
+    id: 2,
+    name: "Backtracking Algorithms",
+    tags: ["javascript", "recursion", "algorithms"],
+    date: "5 July, 2020",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--RkPF7z28--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/zei9kuq0isaw777zznjf.jpg",
+    link: "https://dev.to/jcorley44/backtracking-algorithms-40p6",
+  },
+  {
+    id: 3,
+    name: "React",
+    tags: ["react", "frameworks"],
+    date: "12 July, 2020",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--SrTyeLI8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/bufkt98asyf30zsvfzdz.jpg",
+    link: "https://dev.to/jcorley44/understanding-reactjs-1h44",
+  },
+];
---
+++ src/components/WorkData.js
@@ -0,0 +1,61 @@
+export const Work = [
+  {
+    id: 1,
+    name: "Personal Website",
+    description:
+      "Building a website to function as my resume with links to all of my work.",
+    tags: ["react", "styledComponents", "react-icons"],
+
+    demo: "https://jcorley44.netlify.app/",
+    github: "",
+  },
+  {
+    id: 2,
+    name: "E-Commerce Site (Front End)",
+    description:
+      "Practice building a FullStack web application. Currently fixing minor CSS issues.",
+    tags: ["react", "mysql", "express, node"],
+
+    demo: "",
+    github: "https://github.com/JCorley44/graceshopper-frontend",
+  },
+  {
+    id: 3,
+    name: "E-Commerce Site (Back End)",
+    description: "Practice building a FullStack web application.",
+    tags: ["react", "api", "sass"],
+
+    demo: "",
+    github: "https://github.com/JCorley44/graceshopper-backend",
+  },
+  {
+    id: 4,
+    name: "Sidebar Menu (Coming Soon)",
+    description:
+      "A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+    tags: ["react", "styledComponents", "react-router"],
+
+    demo: "",
+    github: "",
+  },
+  {
+    id: 5,
+    name: "Todo App(React + Redux) (Coming Soon)",
+    description:
+      "A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    tags: ["react", "redux", "framer-motion"],
+
+    demo: "",
+    github: "",
+  },
+  {
+    id: 6,
+    name: "Responsive Navigation Menu (Coming Soon)",
+    description:
+      "A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    tags: ["react", "react-router", "css", "react-icons"],
+
+    demo: "",
+    github: "",
+  },
+];
